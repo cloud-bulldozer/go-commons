cdf0fb3811e813b7d301ed5b757cab1391e476cc
Raul Sevilla <rsevilla@redhat.com>
2025-12-16 10:59:42 +0100

    Address comments
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- ocp-metadata/ocp-metadata.go
+++ ocp-metadata/ocp-metadata.go
@@ -119,23 +119,24 @@ func (meta *Metadata) GetPrometheus() (string, string, error) {
 	return prometheusURL, prometheusToken, err
 }
 
-// GetCurrentPodCount returns the number of running pods across on nodes matching the given labelSelector
-func (meta *Metadata) GetCurrentPodCount(labelSelector string) (int, error) {
+// GetCurrentPodCount returns the number of running pods on nodes matching the given label selector
+func (meta *Metadata) GetCurrentPodCount(nodeLabelSelector string) (int, error) {
 	var podCount int
-	nodeList, err := meta.connector.ClientSet().CoreV1().Nodes().List(context.TODO(), metav1.ListOptions{LabelSelector: labelSelector})
+	mapList := make(map[string]bool)
+	nodeList, err := meta.connector.ClientSet().CoreV1().Nodes().List(context.TODO(), metav1.ListOptions{LabelSelector: nodeLabelSelector})
 	if err != nil {
 		return podCount, err
+	}
+	for _, n := range nodeList.Items {
+		mapList[n.Name] = true
 	}
 	podList, err := meta.connector.ClientSet().CoreV1().Pods(metav1.NamespaceAll).List(context.TODO(), metav1.ListOptions{FieldSelector: "status.phase=" + running})
 	if err != nil {
 		return podCount, err
 	}
 	for _, pod := range podList.Items {
-		for _, node := range nodeList.Items {
-			if pod.Spec.NodeName == node.Name {
-				podCount++
-				break
-			}
+		if _, ok := mapList[pod.Spec.NodeName]; !ok {
+			podCount++
 		}
 	}
 	return podCount, nil
